7b0b2fcf60b7c26efb2675d04913b0b9129dfb3a
D.Kozlov <novolan2014@gmail.com>
2023-11-15 09:29:27 +0200

    add route for unit_aparature_exist and unit_aparature_need

---
+++ route/route-unit-aparature-exist.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response, Router } from 'express';
+import UnitAparatureNeedDb from '../database/db_requests/unit-aparature-exist-request';
+import CreateConnectionUnitAparature from '../domain/Connection/CreateUnitAparature';
+
+
+const router: Router = express.Router()
+
+router.get('/unit-aparatures/:id', async (req: Request, res: Response) => {
+    const id_unit = Number(req.params.id)
+    const data = await UnitAparatureNeedDb.getAllUnitAparature(id_unit)
+
+    res.json(data)
+})
+
+router.patch('/unit-aparatures/:id', async (req: Request, res: Response) => {
+    const id_unit_aparature = Number(req.params.id)
+    const {id_unit, id_aparature, count} = req.body
+
+    const unit_aparature: CreateConnectionUnitAparature = {
+        id_unit: id_unit,
+        id_aparature: id_aparature,
+        count: count
+    }
+
+    const data = await UnitAparatureNeedDb.editUnitAparatureById(id_unit_aparature, unit_aparature)
+
+    res.json(data)
+})
+
+router.delete('/unit-aparature/:id', async (req: Request, res: Response) => {
+    const id_unit_aparature = Number(req.params.id)
+    const data = await UnitAparatureNeedDb.removeUnitAparatureById(id_unit_aparature)
+
+    res.json(data)
+})
+
+
+export default router
--- service/server.ts
+++ service/server.ts
@@ -1,19 +1,28 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+
 import router_aparature from '../route/route-aparature'
 import router_units from '../route/route-units'
+import router_aparature_exist_units from '../route/route-unit-aparature-exist' 
+import router_aparature_need_units from '../route/route-unit-aparature-need' 
+
 import db_init from '../database/db_init';
 import init_config from '../database/db_config';
 import { Sequelize } from 'sequelize';
 import bodyParser = require('body-parser');
+
 
 dotenv.config()
 
 const app: Express = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 ​app.use(express.json())
-app.use('/api_v1', router_aparature)
-app.use('/api_v2', router_units)
+
+app.use('/api/v1', router_aparature)
+app.use('/api/v2', router_units)
+app.use('/api/v3', router_aparature_exist_units)
+app.use('/api/v4', router_aparature_need_units)
+
 
 const port = process.env.SERVER_PORT
 const host = process.env.SERVER_HOST
